3e258845402befa4fc4eedf8491a60a0489fdc0a
Julia Castellano <juliacastellano@Julias-MacBook-Pro.local>
2018-02-06 14:15:25 -0500

    Tried parsing json data

--- oauth-twitter.py
+++ oauth-twitter.py
@@ -1,6 +1,7 @@
 
 from requests_oauthlib import OAuth1Session
 import secrets
+import json
 
 client_key = secrets.api_key
 client_secret = secrets.api_secret
@@ -84,9 +85,11 @@ oauth = OAuth1Session(client_key,
                           resource_owner_key=resource_owner_key,
                           resource_owner_secret=resource_owner_secret)
 r = oauth.get(protected_url)
-print (r.text)
+# print (r.text)
 
 protected_url = 'https://api.twitter.com/1.1/search/tweets.json'
 params = {'q':'food'}
 r = oauth.get(protected_url, params=params)
-print (r.text)
+r_text = r.text
+loaded_text = json.loads(r_text)
+print (loaded_text["statuses"][0]["text"])
